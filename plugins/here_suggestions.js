8ccb565222c89a1d098d3c6fd4756de202ead779
Erik Anderson <eanders@pobox.com>
2017-03-15 19:18:23 -0400

    Adding here.com autosuggestions API as a pluggin.

--- plugins/here_suggestions.js
+++ plugins/here_suggestions.js
@@ -41,18 +41,18 @@ parseUri.options = {
 };
 /************************************* */
 
-class HereCOM extends Plugin {
+class HereCOMSuggestions extends Plugin {
     constructor(name, app_id, app_code) {
         super(name);
         this.app_id = app_id;
         this.app_code = app_code;
-        this.request_host_full = 'https://places.cit.api.here.com/places/v1/discover/search';
+        this.request_host_full = 'https://places.cit.api.here.com/places/v1/autosuggest';
     }
 
     query(sText, callback, additional_params) {
-        console.log("here_laces query text=", sText, "additional_params=", additional_params);
+        console.log("here.com query text=", sText, "additional_params=", additional_params);
         if ( ! additional_params.bounds ) {
-            console.error("ERROR: here_places location search requires boundary boxes!");
+            console.error("ERROR: HERE location search requires boundary boxes!");
             return;
         }
 
@@ -63,7 +63,7 @@ class HereCOM extends Plugin {
 
         var _parsedRequest = parseUri(sFullURL);
 
-        console.log("here_places _parsedRequest=", _parsedRequest, "\n\n\n");
+        console.log("here.com _parsedRequest=", _parsedRequest, "\n\n\n");
 
         request(_parsedRequest.source, function (error, response, body) {
             var cache = [];
@@ -80,22 +80,22 @@ class HereCOM extends Plugin {
             }, 2);
 
             // Delete me ********
-            console.log('here_places error:', error); // Print the error if one occurred
-            console.log('here_places statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('here_places body:', body); // Print the HTML for the here geocode response
-            log.info("here_places body=" + body);
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the here geocode response
+            log.info("here_suggestions body=" + body);
             // *******************
 
             if (response.statusCode != 200) {
                 var cache = [];
-                log.error('ERROR: here_places received bad statusCode:' + response.statusCode + ',  statusMessage:'+ response.statusMessage +
+                log.error('ERROR: Received bad statusCode:' + response.statusCode + ',  statusMessage:'+ response.statusMessage +
                           ' responseObj:' +  sResponseBody);
                 callback([]);
             } else {
                 try {
                     this.query_callback(JSON.parse(body), callback);
                 } catch (err) {
-                    log.error("ERROR: here_places there was an error parsing body:\n" + body);
+                    log.error("ERROR: There was an error parsing body:\n" + body);
                     log.error(err);
                 }
             }
@@ -110,79 +110,96 @@ class HereCOM extends Plugin {
             places: []
         };
 
-        var nLength = oBody.results.items.length;
+        var nLength = oBody.results.length;
 
         console.log("Received nLength=" + nLength + " items");
 
         var _that = this;
 
-        oBody.results.items.forEach(function(oValue){
+        oBody.results.forEach(function(oValue){
             console.log(oValue);
 
             // https://developer.here.com/rest-apis/documentation/places/topics_api/media-type-place.html
-            if ( oValue.category.type === "urn:nlp-types:category" )  {
+            if ( oValue.type === "urn:nlp-types:place" )  {
                 let oNewPlace = {
                     "@type": "Place",  // https://schema.org/Place
                     "name": oValue.title,
-                    additionalProperty: []
-                 };
-
-                 if ( oValue.vicinity ) {
-                     oNewPlace.address = {
-                        "@type": "PostalAddress",  // https://schema.org/address
-                        "addressLocality": oValue.vicinity
-                     }
-                 }
-
-                 oNewPlace.additionalProperty.push ( {
+                    "description": oValue.highlightedTitle,
+                    additionalProperty: [{
                          "@type": "PropertyValue",  // https://schema.org/PropertyValue
-                         "name" : "href",
-                         "url" : oValue.href
-                 });
+                         "name" : "category",
+                         "value": oValue.type
+                    }]
+                };
+
+                if ( oValue.vicinity ) {
+                    oNewPlace.address = {
+                       "@type": "PostalAddress",  // https://schema.org/address
+                       "addressLocality": oValue.vicinity
+                    }
+                }
+
+                
+                oNewPlace.additionalProperty.push ( {
+                    "@type": "PropertyValue",  // https://schema.org/PropertyValue
+                    "name" : "href",
+                    "url" : oValue.href
+                });
 
                 oNewPlace.additionalProperty.push ( {
-                         "@type": "PropertyValue",  // https://schema.org/PropertyValue
-                         "name" : "propertyID",
-                         "value" : oValue.id
+                    "@type": "PropertyValue",  // https://schema.org/PropertyValue
+                    "name" : "propertyID",
+                    "value" : oValue.id
                 });
 
                 if ( oValue.icon ) {
                     // This is the HERE category icon
-                     oNewPlace.image = oValue.icon
+                    oNewPlace.image = oValue.icon
                 }
 
-                if ( oValue.category ) {
-                    // https://developer.here.com/rest-apis/documentation/places/topics_api/media-type-category.html
-                     oNewPlace.additionalProperty.push ({
-                         "@type": "PropertyValue",  // https://schema.org/PropertyValue
-                         "name" : "category",
-                         "description" : oValue.category.title,
-                         "value": oValue.category.id,
-                         "url" : oValue.category.href,
-                         "alternateName" : oValue.category.type
-                     });
+                if ( oValue.position ) {
+                    // https://schema.org/GeoCoordinates
+                    let oGeoCordinates = {
+                        "@type": "GeoCoordinates",
+                        "latitude" : oValue.position[0],
+                        "longitude" : oValue.position[1]
+                    };
+                    oNewPlace.geo = oGeoCordinates;
                 }
 
-                if ( oValue.distance ) {
-                     oNewPlace.additionalProperty.push ( {
-                         "@type": "PropertyValue",  // https://schema.org/PropertyValue
-                         "name" : "distance",
-                         "value" : oValue.distance
-                     });
+                if ( oValue.bbox ) {
+                    let sCoordinates = oValue.bbox[1] + "," + oValue.bbox[0] + " " + oValue.bbox[3] + "," + oValue.bbox[2]
+                    oNewPlace.additionalProperty.push ( {
+                            "@type": "GeoShape",  // https://schema.org/GeoShape
+                            "box" : sCoordinates  // https://schema.org/box
+                    });
                 }
-
-                // https://schema.org/GeoCoordinates
-                let oGeoCordinates = {
-                    "@type": "GeoCoordinates",
-                    "latitude" : oValue.position[0],
-                    "longitude" : oValue.position[1]
-                };
-                oNewPlace.geo = oGeoCordinates;
 
                 oResult.places.push(oNewPlace);
+            } else if ( oValue.type === "urn:nlp-types:autosuggest" )  {
+                // https://developer.here.com/rest-apis/documentation/places/topics_api/media-type-autosuggest.html
+                let oNewPlace = {
+                    "@type": "Place",  // https://schema.org/Place
+                    "name": oValue.title,
+                    "description": oValue.highlightedTitle,
+                    additionalProperty: [{
+                         "@type": "PropertyValue",  // https://schema.org/PropertyValue
+                         "name" : "category",
+                         "value": oValue.type
+                     },{
+                         "@type": "PropertyValue",  // https://schema.org/PropertyValue
+                         "name" : "completion",
+                         "value": oValue.completion
+                    },{
+                         "@type": "PropertyValue",  // https://schema.org/PropertyValue
+                         "name" : "href",
+                         "url" : oValue.href
+                    }]
+                 };
+
             } else {
                 log.warn("Unknown category.type" + JSON.stringify(oValue, null, 2));
-                console.error( "Unknown category.type" + JSON.stringify(oValue, null, 2) );
+                console.error( "Unknown type" + JSON.stringify(oValue, null, 2) );
             }
         });
 
@@ -192,4 +209,4 @@ class HereCOM extends Plugin {
     }
 }
 
-module.exports = HereCOM
+module.exports = HereCOMSuggestions
